d97b276a38cd8838043e84f742d6a2e644412c9d
David Elizondo <david.elizondo@schoolmint.net>
2024-09-26 16:26:36 -0600

    initial commit

---
+++ isolate_json_parser/lib/src/annotation.dart
@@ -0,0 +1,3 @@
+class IsolateJsonParser {
+  const IsolateJsonParser();
+}
---
+++ isolate_json_parser_generator/lib/isolate_json_parser_generator.dart
@@ -0,0 +1,7 @@
+library isolate_json_parser_generator;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
